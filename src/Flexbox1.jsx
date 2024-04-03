d2bc932dbe35adf18288d32fdded16e2f709cf47
farindeoluwabunmi <farindeoluwabunmi497@gmail.com>
2024-04-03 05:13:29 -1100

    my react flexbox assignment

---
+++ src/Flexbox1.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+import "./Flexbox.css"
+function Flexbox1(){
+return(
+  <div className="container-css">
+    <div className="container-flex">
+    <h1>Example of grid 4by4 row and column.</h1>
+    </div>
+ 
+ <div className="grid-container">
+ <h1 className="grid1" >ADE</h1>
+ <h1  className="grid2" >BUKADE</h1>
+ <h1  className="grid3">TOBI</h1>
+ <h1 className="grid4">shade</h1>
+ <h1 className="grid5" >dupe</h1>
+ <h1  className="grid6" >HERMES</h1>
+ <h1  className="grid7">TADE</h1>
+ <h1 className="grid8">OLA</h1>
+ <h1 className="grid9">TEA</h1>
+ <h1  className="grid10">COFFEE</h1>
+ <h1  className="grid11">WATER</h1>
+ <h1 className="grid12">SODA</h1>
+ <h1 className="grid13">CAFFINE</h1>
+ <h1  className="grid14" >COCOA</h1>
+ <h1  className="grid15">CIDER</h1>
+ <h1 className="grid16">SPIRIT</h1>
+  
+ </div>
+ </div>
+)
+}
+
+export default Flexbox1;
